f6bc086cd0b418aff7f4d25478fdd16a1846a8ca
nikolajpister <nikolaj.pister@googlemail.com>
2019-10-18 08:53:49 +0200

    clean up

--- happy-numbers/src/main/java/com/np/happynumbers/Main.java
+++ happy-numbers/src/main/java/com/np/happynumbers/Main.java
@@ -1,6 +1,5 @@
 package com.np.happynumbers;
 
-import java.util.List;
 
 public class Main {
 
@@ -8,9 +7,8 @@ public class Main {
     private static final Integer END = 20;
 
     public static void main(String[] args) {
-        System.out.println("happy numbers between " + START + " and " + END);
-        List<Integer> result = new HappyNumbersCalc().getHappyNumbersBetween(START, END);
-        result.stream().forEach(System.out::println);
+        System.out.println(String.format("happy numbers between %s and %s are:", START, END));
+        
     }
 
 
